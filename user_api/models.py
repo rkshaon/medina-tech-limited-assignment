8fbe742fee57227e530f4a48bd525828b7e85b4b
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-09-24 00:44:33 +0600

    user api - model - done

--- user_api/models.py
+++ user_api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    ADMIN = 1
+    VENDOR = 2
+    CUSTOMER =3
+      
+    ROLE_CHOICES = (
+        (ADMIN, 'Admin'),
+        (VENDOR, 'Vendor'),
+        (CUSTOMER, 'Customer'),
+    )
+    
+    name = models.CharField(max_length=255, null=True, blank=True)
+    email = models.EmailField(max_length=254, unique=True)
+    username = models.CharField(max_length=30, unique=True)
+    password = models.CharField(max_length=255)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, blank=True, null=True)
+
+    
