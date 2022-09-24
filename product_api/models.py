f9a353c4fa1e3652390ba3bbb6bde7b5cfa50261
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-09-25 00:25:12 +0600

    product api - model - updated

--- product_api/models.py
+++ product_api/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=255, blank=False, null=False)
     image = models.ImageField(upload_to='products', blank=True)
+    product_type = models.ForeignKey('weather_api.WeatherType', blank=True, null=True, on_delete=models.CASCADE)
     quantity = models.FloatField(default=0.0, blank=True, null=True)
     added_by = models.ForeignKey('user_api.User', on_delete=models.CASCADE, related_name='added_by')
     edited_by = models.ForeignKey('user_api.User', on_delete=models.CASCADE, blank=True, null=True, related_name='updated_by')
