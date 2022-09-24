8cb883b77cfbc1d69c1caff8c2b2b55720ab2f09
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-09-24 22:53:08 +0600

    weather api - get weather type

--- weather_api/urls.py
+++ weather_api/urls.py
@@ -5,4 +5,5 @@ from weather_api import views
 
 urlpatterns = [
     path('', views.get_all_weather_type),
+    path('<pk>', views.handle_weather_type_get_or_update_or_delete),
 ]
--- weather_api/views.py
+++ weather_api/views.py
@@ -21,3 +21,35 @@ def get_all_weather_type(request):
         'status': True,
         'data': data,
     })
+
+
+def get_weather_type(request, pk):
+    user = auth_user(request)
+
+    try:
+        weather_type = WeatherType.objects.get(id=pk, is_deleted=False)
+    except Exception as e:
+        return Response({
+            'status': False,
+            'message': 'Weather type does not found!'
+        }, status=status.HTTP_404_NOT_FOUND)
+
+    weather_type_serializer = WeatherTypeSerializer(weather_type, many=False)
+    data = weather_type_serializer.data
+    
+    return Response({
+        'status': True,
+        'data': data,
+    })
+
+
+WEATHER_TYPE_GET_OR_UPDATE_OR_DELETE = {
+    'GET': get_weather_type,
+    # 'DELETE': delete_product,
+    # 'PUT': update_product,
+}
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def handle_weather_type_get_or_update_or_delete(request, pk):
+    return WEATHER_TYPE_GET_OR_UPDATE_OR_DELETE[request.method](request, pk)
