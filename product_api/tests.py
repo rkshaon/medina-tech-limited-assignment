882e68313741163bfa37ce9061fe853d975bf022
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-09-27 00:22:56 +0600

    product api - url - test

--- product_api/tests.py
+++ product_api/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
 
-# Create your tests here.
+from product_api.views import get_all_product
+from product_api.views import add_product
+from product_api.views import recommended_product
+from product_api.views import handle_product_get_or_update_or_delete
+
+
+class TestUrls(SimpleTestCase):
+    def test_all_product_url_resolves(self):
+        url = reverse('all_products')
+        self.assertEquals(resolve(url).func, get_all_product)
+    
+    def test_add_product_url_resolves(self):
+        url = reverse('add_product')
+        self.assertEquals(resolve(url).func, add_product)
+    
+    def test_recommended_product_url_resolves(self):
+        url = reverse('receommended_product')
+        self.assertEquals(resolve(url).func, recommended_product)
+    
+    def test_get_or_update_or_delete_product_url_resolves(self):
+        url = reverse('get_or_update_or_delete_product', args=['some-id'])
+        self.assertEquals(resolve(url).func, handle_product_get_or_update_or_delete)
--- product_api/urls.py
+++ product_api/urls.py
@@ -4,8 +4,8 @@ from product_api import views
 
 
 urlpatterns = [
-    path('', views.get_all_product),
-    path('add', views.add_product),
-    path('weather-recommended', views.recommended_product),
-    path('<pk>', views.handle_product_get_or_update_or_delete),    
+    path('', views.get_all_product, name='all_products'),
+    path('add', views.add_product, name='add_product'),
+    path('weather-recommended', views.recommended_product, name='receommended_product'),
+    path('<pk>', views.handle_product_get_or_update_or_delete, name='get_or_update_or_delete_product'),
 ]
