26a1730bfa6bad0619cdede907e99c6907fa0e09
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-09-27 00:18:27 +0600

    weather api - url - test

--- weather_api/tests.py
+++ weather_api/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
 
-# Create your tests here.
+from weather_api.views import get_all_weather_type
+from weather_api.views import add_weather_type
+from weather_api.views import handle_weather_type_get_or_update_or_delete
+
+
+class TestUrls(SimpleTestCase):
+    def test_all_weather_types_url_resolves(self):
+        url = reverse('all_weather_types')
+        self.assertEquals(resolve(url).func, get_all_weather_type)
+    
+    def test_add_weather_url_resolves(self):
+        url = reverse('add_weather')
+        self.assertEquals(resolve(url).func, add_weather_type)
+    
+    def test_get_or_update_or_delete_weather_url_resolves(self):
+        url = reverse('get_or_update_or_delete_weather', args=['some-id'])
+        self.assertEquals(resolve(url).func, handle_weather_type_get_or_update_or_delete)
--- weather_api/urls.py
+++ weather_api/urls.py
@@ -4,7 +4,7 @@ from weather_api import views
 
 
 urlpatterns = [
-    path('', views.get_all_weather_type),
-    path('add', views.add_weather_type),
-    path('<pk>', views.handle_weather_type_get_or_update_or_delete),
+    path('', views.get_all_weather_type, name='all_weather_types'),
+    path('add', views.add_weather_type, name='add_weather'),
+    path('<pk>', views.handle_weather_type_get_or_update_or_delete, name='get_or_update_or_delete_weather'),
 ]
