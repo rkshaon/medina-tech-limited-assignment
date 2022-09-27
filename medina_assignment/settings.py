a578da4ba98b990098ccd9bf528a80bf89da0e46
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-09-27 10:52:48 +0600

    Update settings.py

--- medina_assignment/settings.py
+++ medina_assignment/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_filters',
     'user_api',
     'product_api',
     'weather_api',
